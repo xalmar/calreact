ca8bc737589f307373739437d473c1ca16e07063
Marat <xalmar@gmail.com>
2017-06-07 20:57:54 +0300

    Add react datetime

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,30 +1,43 @@
 var AppointmentForm = React.createClass({
-	handleChange: function(e) {
-		var name = e.target.name;
-		obj = {};
-		obj[name] = e.target.value;
-		this.props.onUserInput(obj);
-	},
+    handleChange: function(e) {
+        var name = e.target.name;
+        obj = {};
+        obj[name] = e.target.value;
+        this.props.onUserInput(obj);
+    },
 
-	handleSubmit: function(e) {
-		e.preventDefault();
-		this.props.onFormSubmit();
-	},
+    handleSubmit: function(e) {
+        e.preventDefault();
+        this.props.onFormSubmit();
+    },
 
-	render: function() {
-		return (
-			<div>
-				<h2>Make a new appointment</h2>
-				<form onSubmit={this.handleSubmit} >
-					<input name='title' placeholder ='Appointment Title' 
-						value={this.props.input_title} 
-						onChange={this.handleChange} />
-					<input name='apt_time' placeholder ='Date and Time' 
-						value={this.props.input_apt_time}  
-						onChange={this.handleChange} />
-					<input type='submit' value='Make Appointment' />
-				</form>		
-			</div>
-		);
-	}
+    setAptTime: function(e) {
+        var name = 'apt_time';
+        var obj = {}
+        if (obj[name] = e.toDate()) {
+            this.props.onUserInput(obj);
+        }
+    },
+
+    render: function() {
+        var inputProps = {
+            name: 'apt_time'
+        };
+
+        return (
+            <div>
+                <h2>Make a new appointment</h2>
+                <form onSubmit={this.handleSubmit} >
+                    <input name='title' placeholder ='Appointment Title' 
+                        value={this.props.input_title} 
+                        onChange={this.handleChange} />
+
+                    <Datetime input={false} open={true} inputProps={inputProps}
+                        value={this.props.apt_time} 
+                        onChange={this.setAptTime}/>
+                    <input type='submit' value='Make Appointment' />
+                </form>     
+            </div>
+        );
+    }
 });
